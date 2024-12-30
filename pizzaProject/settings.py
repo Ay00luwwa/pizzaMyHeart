e71332ca4164458e7096e4d7582b9e8ca0e1ca12
Ay00luwwa <ijidakinroayooluwa@gmail.com>
2024-12-31 00:55:46 +0100

    new changes to settings.allowed hosts

--- pizzaProject/settings.py
+++ pizzaProject/settings.py
@@ -12,9 +12,9 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 SECRET_KEY = "django-insecure-cf))(k(zv3!h2fou4uon$)xrmg^u10kd(_&cn0+0ew$6dzw#9&"
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
+DEBUG = False
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ["pizzame.pythonanywhere.com", "localhost", "127.0.0.0.1",]
 
 
 # Application definition
