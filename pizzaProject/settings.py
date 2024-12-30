32eb5e57f12e54a193214dd2157433436695ebd5
Ay00luwwa <ijidakinroayooluwa@gmail.com>
2024-12-31 00:31:25 +0100

    db change

--- pizzaProject/settings.py
+++ pizzaProject/settings.py
@@ -75,23 +75,23 @@ WSGI_APPLICATION = "pizzaProject.wsgi.application"
 # Database
 # https://docs.djangoproject.com/en/5.1/ref/settings/#databases
 
-# DATABASES = {
-#     "default": {
-#         "ENGINE": "django.db.backends.sqlite3",
-#         "NAME": BASE_DIR / "db.sqlite3",
-#     }
-# }
-
 DATABASES = {
-    'default': {
-        'ENGINE': 'django.db.backends.postgresql',
-        'NAME': 'pizza_me',
-        'USER': 'postgres',
-        'PASSWORD': 'godmode1',
-        'HOST': 'localhost', 
-        'PORT': '5432', 
+    "default": {
+        "ENGINE": "django.db.backends.sqlite3",
+        "NAME": BASE_DIR / "db.sqlite3",
     }
 }
+
+# DATABASES = {
+#     'default': {
+#         'ENGINE': 'django.db.backends.postgresql',
+#         'NAME': 'pizza_me',
+#         'USER': 'postgres',
+#         'PASSWORD': 'godmode1',
+#         'HOST': 'localhost', 
+#         'PORT': '5432', 
+#     }
+# }
 
 
 NPM_BIN_PATH = "C:\\Users\\Ayooluwa\\AppData\\Roaming\\npm\\npm.cmd"
